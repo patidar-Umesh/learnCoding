d21949174b1b09d47dad9f3fac816f137356a223
patidar-Umesh <patidarumesh2312@gmail.com>
2024-05-04 15:50:26 +0530

    remove unused import files from some files

--- learnCoding/Frontend/src/components/Instructor/InstructorDashboard/Instructor.jsx
+++ learnCoding/Frontend/src/components/Instructor/InstructorDashboard/Instructor.jsx
@@ -31,7 +31,7 @@ const Instructor = () => {
     getCourseDataWithStats();
     // eslint-disable-next-line react-hooks/exhaustive-deps
 
-  }, [tokan]);
+  }, [token]);
 
   const totalAmount = instructorData?.reduce(
     (acc, curr) => acc + curr.totalAmountGenerated,
