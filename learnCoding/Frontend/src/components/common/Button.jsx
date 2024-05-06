ad596f634e4d67d9cf5859e309f61c37797203ea
patidar-Umesh <157127984+patidar-Umesh@users.noreply.github.com>
2024-05-06 21:49:01 +0530

    Update Button.jsx

--- learnCoding/Frontend/src/components/common/Button.jsx
+++ learnCoding/Frontend/src/components/common/Button.jsx
@@ -14,7 +14,7 @@ const Button = ({
         onClick={onClick}
         type={type}
         disabled={disabled}
-        className={` text-center text-[16px]  flex justify-center items-center bg-richblack-200 rounded-md font-bold
+        className={` text-center text-[16px] px-6 py-3 flex justify-center items-center bg-richblack-200 rounded-md font-bold
         hover:scale-95 transition-all text-black duration-200 ${className}
         `}
         {...props}
