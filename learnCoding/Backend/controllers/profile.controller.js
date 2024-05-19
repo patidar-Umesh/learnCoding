680ada83ef2a34959033ae2e74999256d22cd9d8
patidar-Umesh <patidarumesh2312@gmail.com>
2024-05-19 18:03:36 +0530

    change in profile controller

--- learnCoding/Backend/controllers/profile.controller.js
+++ learnCoding/Backend/controllers/profile.controller.js
@@ -297,7 +297,7 @@ const instructorDashboard = async (req, res) => {
       })
     });
 
-    res.status(200).json({ courses: courseData });
+    return res.status(200).json({ courses: courseData });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
